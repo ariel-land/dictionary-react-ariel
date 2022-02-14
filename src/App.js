5fac9396d028924c53b5750b201c32c478f09146
Ariel Lee <arielslove629@gmail.com>
2022-02-14 23:18:08 +0900

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">DICTIONARY APP</header>
+      <footer className="App-footer">
+        <a
+          href="https://github.com/ariel-land/dictionary-react-ariel"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Ariel Lee{" "}
+      </footer>
     </div>
   );
 }
