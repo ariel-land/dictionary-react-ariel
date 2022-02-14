fef9ff54897a9944c70a3ae366c4b787c0229fe6
Ariel Lee <arielslove629@gmail.com>
2022-02-14 23:05:57 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">DICTIONARY APP</header>
+    </div>
+  );
+}
+
+export default App;
